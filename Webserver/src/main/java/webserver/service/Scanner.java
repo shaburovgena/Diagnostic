c3ace8d65919b7c383b55adefe477985403b1595
shaburovgena <shaburovgena@gmail.com>
2019-02-19 11:55:01 +0300

    Add scanner for searching sensors in subnet

--- Webserver/src/main/java/webserver/service/Scanner.java
+++ Webserver/src/main/java/webserver/service/Scanner.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.net.*;
 import java.util.Enumeration;
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
 
 public class Scanner {
 
@@ -15,7 +17,7 @@ public class Scanner {
     private Set<String> sensors;
     private Set<String> interfaces;
 
-    private void getAllInterfaces() throws SocketException {
+    public void getAllInterfaces() throws SocketException {
         Enumeration e = NetworkInterface.getNetworkInterfaces();
         while (e.hasMoreElements()) {
             NetworkInterface n = (NetworkInterface) e.nextElement();
@@ -27,19 +29,21 @@ public class Scanner {
         }
 
     }
-@Async
+
+    @Async
     public void scan() throws SocketException, UnknownHostException {
 
         InetAddress localhost = InetAddress.getByName(Inet4Address.getLocalHost().getHostAddress());
         byte[] buf = localhost.getAddress();
-        for (int i = 0; i < buf.length; i++) {
-            System.out.println(buf[i]);
-        }
-        for (int i = 1; i < 255; i++) {
-            int port = 23;
-            String host = buf[0] + "." + buf[1] + "." + (buf[2]+256) + "." + i;
+
+        for (int i = 254; i > 0; i--) {
+            int port = 445;
+            String host = (0xff & buf[0]) + "." + (0xff & buf[1]) + "." + (0xff & buf[2]) + "." + i;
             try {
-                Socket socket = new Socket(host, port);
+                Socket socket = new Socket();
+                socket.connect(new InetSocketAddress(host, port), 200);
+
+                socket.close();
                 System.out.println("Connect: " + host);
             } catch (IOException e) {
                 System.out.println("Could not connect on port: " + port + " " + host);
