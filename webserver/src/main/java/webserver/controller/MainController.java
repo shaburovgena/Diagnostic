8e53eca254945e03fb4732715574f3c550ae9e13
shaburovgena <shaburovgena@gmail.com>
2019-09-27 15:06:06 +0300

    Advanced Json

--- webserver/src/main/java/webserver/controller/MainController.java
+++ webserver/src/main/java/webserver/controller/MainController.java
@@ -1,20 +1,20 @@
 package webserver.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import webserver.domain.User;
+import webserver.domain.Views;
 import webserver.repos.GroupRepo;
 import webserver.repos.SensorRepo;
-import webserver.service.UserService;
 
-import javax.validation.Valid;
 import java.util.HashMap;
 
 @Controller
@@ -24,73 +24,51 @@ public class MainController {
     private final SensorRepo sensorRepo;
     @Value("${spring.profiles.active}")
     private String profile;
+    private final ObjectWriter groupWriter;
+    private final ObjectWriter profileWriter;
     private GroupRepo groupRepo;
-    @Autowired
-    private UserService userService;
+
+
     @Autowired
     public MainController(SensorRepo sensorRepo,
-                          UserService userService, GroupRepo groupRepo) {
+                          GroupRepo groupRepo, ObjectMapper mapper) {
         this.sensorRepo = sensorRepo;
-        this.userService = userService;
+
         this.groupRepo = groupRepo;
+
+        ObjectMapper objectMapper = mapper
+                .setConfig(mapper.getSerializationConfig());
+        this.groupWriter = objectMapper
+                .writerWithView(Views.IdNameValueAttribute.class);
+        this.profileWriter = objectMapper
+                .writerWithView(Views.IdNameRoles.class);
+
     }
 
     @GetMapping
     public String main(
             Model model,
             @AuthenticationPrincipal User user
-
-    ) {
+    ) throws JsonProcessingException {
         HashMap<Object, Object> data = new HashMap<>();
-
-        if (user != null) {
-            user.setPassword("");
-            data.put("profile", user);
-            data.put("sensors", sensorRepo.findAll());
-            data.put("groups", groupRepo.findAll());
+        if (user != null && user.getActivationCode() == null) {
+            model.addAttribute("profile", profileWriter.writeValueAsString(user));
+            model.addAttribute("sensors", sensorRepo.findAll());
+            model.addAttribute("groups", groupWriter.writeValueAsString(groupRepo.findAll()));
+            data.put("errorMessage", "Hello");
+            model.addAttribute("frontendData", data);
         } else {
-            data.put("sensors", "");
-            data.put("groups", "");
+            model.addAttribute("profile", "null");
+            model.addAttribute("groups", "[]");
+            model.addAttribute("sensors", "[]");
+            data.put("errorMessage", "Hello");
+            data.put("isLoginForm", "true");
+            model.addAttribute("frontendData", data);
         }
-        model.addAttribute("frontendData", data);
+
         model.addAttribute("isDevMode", "dev".equals(profile));
         return "index";
     }
 
-    @PostMapping("/register")
-    public String register(@RequestParam("passwordConfirm") String passwordConfirm,
-                           @Valid User user,
-                           Model model) {
 
-
-        HashMap<Object, Object> data = new HashMap<>();
-        if (user.getPassword() == null || passwordConfirm == null ||
-                !user.getPassword().equals(passwordConfirm)) {
-            data.put("errorMessage", "Проверьте правильность введенных данных");
-            data.put("isRegisterForm", true);
-            data.put("sensors", "");
-            data.put("groups", "");
-            model.addAttribute("frontendData", data);
-            return "index";
-        }
-        if (userService.addUser(user) != null) {
-            return "redirect:/login?error";
-        }
-        return "redirect:/register";
-    }
-        @GetMapping("/register")
-    public String register(Model model,
-                           @AuthenticationPrincipal User user) {
-        HashMap<Object, Object> data = new HashMap<>();
-
-
-            data.put("profile", user);
-
-            data.put("sensors", "");
-            data.put("groups", "");
-
-        model.addAttribute("frontendData", data);
-        return "index";
-
-    }
 }
